7a28c6ad8b9a05e02bf7f315b55e39b45e260aa3
Carina Wickstrom <carinawi@kth.se>
2022-09-06 15:23:42 +0200

    add files

--- src/LazyHashing.java
+++ src/LazyHashing.java
@@ -1,5 +1,29 @@
+import java.io.*;
+
 public class LazyHashing {
     public static void main(String [ ] args) {
-        System.out.println("hello worl");
+      // real data: /afs/kth.se/misc/info/kurser/DD2350/adk22/labb1
+      //
+
+      try {
+        FileInputStream inputStream
+            = new FileInputStream("exampletext.txt");
+
+        BufferedInputStream buffInputStr
+            = new BufferedInputStream(
+                inputStream);
+
+        while (buffInputStr.available() > 0) {
+
+            char c = (char)buffInputStr.read();
+
+            System.out.println("Char : " + c);
+        }
+
+      }
+      catch(Exception e) {
+
+
+      }
     }
 }
