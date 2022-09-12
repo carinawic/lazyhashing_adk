9c3fcf738ae827faf94202bb8f195d658105282d
Carina Wickström <49478094+carinawic@users.noreply.github.com>
2022-09-12 14:47:03 +0200

    woho

--- src/Konkordans.java
+++ src/Konkordans.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 //import org.apache.commons.lang3.StringUtils;
 
@@ -23,7 +25,38 @@ public class Konkordans {
       int hashedValue = helper.getHash(completeWord);
       System.out.println(hashedValue);
 
+      
 
+
+      try{
+        byte[] read_from_a = readCharsFromFile("a.txt", hashedValue*8, 8);
+        
+        String string_from_a = new String(read_from_a);
+
+        Pattern p = Pattern.compile("[0-9]");
+        Matcher m = p.matcher(string_from_a);
+        if (m.find()){
+          System.out.println(new String("MATCH!!!"));
+
+        }else{
+          System.out.println(new String("NO MATCH!!!"));
+          return;
+        }
+
+
+      } catch (IOException e) {
+            e.printStackTrace();
+      }
+      
 
     }
+
+    private static byte[] readCharsFromFile(String filePath, int seek, int chars) throws IOException {
+      RandomAccessFile file = new RandomAccessFile(filePath, "r");
+      file.seek(seek);
+      byte[] bytes = new byte[chars];
+      file.read(bytes);
+      file.close();
+      return bytes;
+	}
 }
